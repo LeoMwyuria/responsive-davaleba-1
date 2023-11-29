6a8ec80514cded2fa59f456ef78af1b09ce78a0d
Levani Chargeishvili <145695415+LeoMwyuria@users.noreply.github.com>
2023-11-29 18:06:44 +0400

    Add files via upload

---
+++ JavaScript-Davaleba/Davaleba-1.js
@@ -0,0 +1,6 @@
+const saxelebi = [ 'Aurora','Caleb','Isabella','Xavier','Zoey','Gabriel','Lily','Ethan','Savannah','Oliver']
+for (let i = 0; i < saxelebi.length; i++){
+   console.log(saxelebi[i])
+  
+    
+}
---
+++ JavaScript-Davaleba/Davaleba-2.js
@@ -0,0 +1,37 @@
+const cars = [
+    {
+       car_name:' Lamborghini_Gallardo',
+       Release_date: ' 2003',
+       KG: '1501',
+      
+   },
+   {
+       car_name:'Toyota Camry',
+       Release_date: ' 1982',
+       KG: '1497',
+      
+
+   },
+   {
+       car_name:'Tesla Model S',
+       Release_date: ' 2012',
+       KG: '1506',
+       
+   },
+   {
+       car_name:'Ford Mustang GT',
+       Release_date: ' 1964',
+       KG: '1501',
+      
+   },
+   {
+       car_name:'Chevrolet Corvette',
+       Release_date: ' 1953',
+       KG: '1430',
+      
+   }
+
+]
+for ( let i = 0; i< cars.length; i++){
+    console.log(cars[i].car_name + cars[i].Release_date)
+} 
